3879fcb304399c5a0bc18dd8a63cbabe18da6bb1
Sundas-Razzaq <sundasr621@gmail.com>
2026-01-14 23:54:18 +0500

    done working with add, update, read

--- node_with_sql/index.js
+++ node_with_sql/index.js
@@ -2,7 +2,15 @@ const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 //create connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -33,20 +41,83 @@ let createRandomUser = () => {
 // };
 // connection.end();
 
-
+//get users count and render on home page --> crud operation - read
 app.get('/', (req, res) => {
     let q = `select count(*) from users`;
     try {
-        connection.query(q, (err, results, fields) => {
+        connection.query(q, (err, results) => {
             if (err) throw err;
-            console.log('Tables in the database:', results);
-            res.send(results);
+            let usersCount = results[0]['count(*)'];
+            res.render('home.ejs', { usersCount: usersCount });
         });
     } catch (err) {
         console.log(err);
         res.status(500).send('Internal Server Error');
     }
 });
+
+//sohw all users
+app.get('/users', (req, res) => {
+    let q = `select * from users`;
+    try {
+        connection.query(q, (err, results) => {
+            if (err) throw err;
+            let users = results;
+            res.render('users.ejs', { users: results });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+//edit user
+app.get('/user/:id/edit', (req, res) => {
+    let { id } = req.params;
+    let q = `select * from users where userId = '${id}'`;
+    try {
+        connection.query(q, (err, results) => {
+            if (err) throw err;
+            res.render('edit.ejs', { users: results[0] });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+
+//update user
+app.patch('/user/:id', (req, res) => {
+    const { id } = req.params;
+    const { username, password } = req.body;
+    const q1 = `SELECT * FROM users WHERE userId = '${id}'`;
+    connection.query(q1, (err, results) => {
+        if (err) return res.status(500).send('DB error');
+        const user = results[0];
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        if (password !== user.password) {
+            return res.redirect(`/user/${id}/edit`);
+        }
+        const q2 = `UPDATE users SET username='${username}' WHERE userId='${id}'`;
+        connection.query(q2, (err) => {
+            if (err) return res.status(500).send('Update failed');
+            return res.redirect('/users');
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
